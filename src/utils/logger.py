9bae05000f5ff887a392b20fb982f0b02e98fe0b
DS-expert <workmy.chd@gmail.com>
2025-11-27 12:48:36 +0500

    log(logger): Add new line to the logger.py which will control the logging generation in log/ folder

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -7,12 +7,13 @@ LOGS_DIR.mkdir(parents=True, exist_ok=True)
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 LOG_FILE_PATH = LOGS_DIR / LOG_FILE
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    level=logging.INFO,
-    format="[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d - %(message)s",
-    datefmt= "%Y-%m-%d %H:%M:%S"
-)
+if not logging.getLogger().handlers:
+    logging.basicConfig(
+        filename=LOG_FILE_PATH,
+        level=logging.INFO,
+        format="[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d - %(message)s",
+        datefmt= "%Y-%m-%d %H:%M:%S"
+    )
 
 def get_logger(name: str) -> logging.Logger:
     """
